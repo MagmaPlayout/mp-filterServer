1d1b8a9f280160aeb16954ccd7719d52633bc7e0
Luis.O.M <luismunoz.dh@gmail.com>
2016-10-14 14:11:41 -0300

    "crud"

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require ("express");
 var app = new express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-
+var filter = require("./filter.js");
+var config = require("./config.js")
 var Log = require ("log"),
 	log = new Log("debug");
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || config.app.port;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -14,13 +15,51 @@ app.get('/',function(req,res){
 	res.redirect('index.html');
 });
 
+//---------------------------------------------------
+//-------------- SOCKETS ----------------------------
+//---------------------------------------------------
 io.on('connection',function(socket){
-	log.info("Un cliente se conecto" );
-	socket.on('filter',function(filterHtml){
+	log.info("Un cliente se conecto." );
+
+	filter.getAll(function(error,reply){
+		if(!error){
+			socket.emit("filterList", reply);
+		}
+
+	});
+
+	//Espera por todos los html que el usuario va agregando al video
+	socket.on('filterEditor',function(filterHtml){
+
 		log.info("recibido de un cliente: " + filterHtml );
-		socket.broadcast.emit('filter',filterHtml);
-	})
-})
+
+		//Esta linea envia los cambios al socket que esta escuchando en melt
+		socket.broadcast.emit('filterChanged',filterHtml);
+	});
+
+	//actualizo un filtro.
+	socket.on("filterSave", function(filterJson){
+
+		filter.update(filterJson);
+
+	});
+
+	//Agrego nuevo filtro
+	socket.on("filterAdd", function(filterJson){
+		filter.add(filterJson);
+		filter.getAll(function(error,reply){
+			if(!error){
+				socket.emit("filterList", reply);
+			}
+
+		});
+	});
+});
+
+//---------------------------------------------------
+//---------------------------------------------------
+//---------------------------------------------------
+
 
 http.listen(port,function(){
 	log.info("Filter editor - Bienvenidos gatos!");
